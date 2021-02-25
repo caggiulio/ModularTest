ed4023b4b04677e6798c9568106da55af8a8dfa8
Nunzio Giulio Caggegi <>
2021-02-25 21:31:21 +0100

    add test class

---
+++ DevPods/DummyCore/DummyCore/Module/DummyTest.swift
@@ -0,0 +1,14 @@
+//
+//  DummyTest.swift
+//  Pods-ModularTest
+//
+//  Created by Nunzio Giulio Caggegi on 25/02/21.
+//
+
+import Foundation
+
+public class DummyTest {
+    public func test() {
+        print("test")
+    }
+}
