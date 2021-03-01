2211d334ae9f324f17fdc4f5894c81b2c332f0bb
Nunzio Giulio Caggegi <>
2021-03-01 19:15:34 +0100

    added folders

---
+++ DevPods/DummyCore/DummyCore/Module/Test3/Test3.swift
@@ -0,0 +1,19 @@
+//
+//  Test3.swift
+//  DummyCore
+//
+//  Created by Nunzio Giulio Caggegi on 01/03/21.
+//
+
+import Foundation
+
+public class DummyTest3 {
+    
+    public init() {
+        
+    }
+    
+    public func test3() {
+        print("test3")
+    }
+}
